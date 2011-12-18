5bd5090e00c5655c5dd5dbc268714a56df252f3d
Andrey <to.merge@gmail.com>
2011-12-18 11:09:25 +0600

    add Message class

---
+++ reporter/Message.h
@@ -0,0 +1,16 @@
+//
+//  Message.h
+//  reporter
+//
+//  Created by Andrey Putilov on 12/18/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Message : NSObject
+
+@property (copy)NSString *text;
+@property (retain)NSMutableArray *photos;
+
+@end
---
+++ reporter/Message.m
@@ -0,0 +1,15 @@
+//
+//  Message.m
+//  reporter
+//
+//  Created by Andrey Putilov on 12/18/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Message.h"
+
+@implementation Message
+
+@synthesize text, photos;
+
+@end
