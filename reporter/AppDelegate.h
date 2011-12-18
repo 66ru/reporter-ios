a0d4170e97f1101a64205fa2d6d5166f73dd687b
Andrey <to.merge@gmail.com>
2011-12-18 10:59:28 +0600

    first commit

---
+++ reporter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  reporter
+//
+//  Created by Andrey Putilov on 12/18/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
